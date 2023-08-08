b1849c9864c9de923c56850fefa3a356201a0fde
Jhonatan Prestes <90355675+jprestes@users.noreply.github.com>
2023-08-08 19:44:41 -0300

    Add files via upload

---
+++ exercicio1.js
@@ -0,0 +1,6 @@
+function exibirAlerta() {
+    //alert('ALERTA-ALERTA');
+    //document.getElementById('box').innerHTML = 'MEU ALERTA DE UMA FUNÇÃO'
+    //console.log('MINHA PRIMEIRA MENSAGEM NO CONSOLE')
+    confirm('Voce concorda ?')
+}
---
+++ exercicio2.js
@@ -0,0 +1,16 @@
+function funcaoTeste() {
+    document.getElementById('box1').style.backgroundColor = 'blue' ;
+
+    document.getElementsByClassName('paragrafo')[0].style.backgroundColor = 'green';
+    document.getElementsByClassName('paragrafo')[1].style.backgroundColor = 'yellow';
+    document.getElementsByClassName('paragrafo')[2].style.backgroundColor = 'red';
+
+}
+
+function carregandoPagina() {
+    alert('CUIDADO !!!!')
+} 
+
+function campoObrigatorio() {
+    alert('Favor preencher o CPF')
+}
